be7dca2e60997f31fd370d1d5023e50ae66c57a0
godidier <godidier@gmail.com>
2023-04-02 00:28:33 +0900

    wrote sentence sim (almost by hand)

--- entity.py
+++ entity.py
@@ -1,4 +1,9 @@
 import requests
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModel
+
 
 class EntityLinker:
     """Helper class to disambiguate entities
@@ -9,11 +14,11 @@ class EntityLinker:
             + "&language={1}"
             + "format=json"
 
-    def __init__():
-        pass 
+    def __init__(self):
+        self.cos_sim = nn.CosineSimilarity(dim=1, 1e-6)
     
 
-    def disambiguate(ent_name, sentences):
+    def disambiguate(self, ent_name, sentences):
         """
         :param ent_name: the name of the entity to disambiguate
         :param sentences: a list of sentence in which the `ent_name` appears
@@ -37,8 +42,26 @@ class EntityLinker:
             return None
 
 
-    def sentence_simscore(sentence1, sentence2):
+    def sentence_simscore(self, sentence1, sentence2):
         """
         Compute the a similarity score between two sentences
         """
-        pass
+        sentences = [sentence1, sentence2]
+        tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+        model = AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+        encoded_input1 = tokenizer(sentence1, padding=True, truncation=True, return_tensors='pt')
+        encoded_input2 = tokenizer(sentence2, padding=True, truncation=True, return_tensors='pt')
+        with torch.no_grad():
+            model_output1 = model(**encoded_input1)
+            model_output2 = model(**encoded_input2)
+        sentence_embeddings1 = self.mean_pooling(model_output1, encoded_input['attention_mask'])
+        sentence_embeddings2 = self.mean_pooling(model_output2, encoded_input['attention_mask'])
+        sentence_embeddings1 = F.normalize(sentence_embeddings1, p=2, dim=1)
+        sentence_embeddings2 = F.normalize(sentence_embeddings2, p=2, dim=1)
+        return self.cos_sim(sentence_embeddings1, sentence_embeddings2)
+
+
+    def mean_pooling(model_output, attention_mask):
+        token_embedding = model_output[0] #First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embedding.size()).float()
+        return torch.sum(token_embedding * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(-1), min=1e-9)
