e2377e12acc536d7ee34cfe8fbcd6c44da64eb80
godidier <godidier@gmail.com>
2023-04-03 21:12:05 +0900

    copy kbuild and entity from prev

---
+++ entities.py
@@ -0,0 +1,103 @@
+import json
+
+class NodeType:
+    ORG = "ORG"
+    PERSON = "PERSON"
+    PLACE = "GPE"
+    Set = {"ORG", "PERSON", "GPE", "LOCATION"}
+    #Set = [NodeType.ORG, NodeType.PERSON, NodeType.PLACE}"ORG", "PERSON", "GPE"]
+
+
+class EdgeType:
+    TRADE = "TRADE"
+    OTHER = "OTHER"
+    Set = ["TRADE", "OTHER"]
+
+
+class Node:
+    """
+    ent_count is the number of time the entity was identify in the corpus
+    """
+
+    def __init__(self, label, ner_type, count=0):
+        self.id = self.id_from_label(label)
+        self.ner_type = ner_type
+        self.label = label 
+        self.count = count
+        self.aliases = []
+
+
+    def id_from_label(self, label=None):
+        id_str = label if label is not None else self.label
+        id_str = id_str.lower()
+        if id_str.startswith("the "): 
+            id_str = id_str[4:]
+        elif id_str.startswith("an "):
+            id_str = id_str[3:]
+        # TODO is below necessary ? how to make it efficient ? chain .replace?
+        id_str = id_str.replace("'s", "") 
+        id_str = id_str.replace(".", "") 
+        id_str = id_str.replace(",", "") 
+        id_str = id_str.replace("(", "") 
+        id_str = id_str.replace(")", "") 
+        id_str = id_str.replace("<", "") 
+        id_str = id_str.replace(">", "") 
+        id_str = id_str.replace("\"", "") 
+        id_str = id_str.replace("/", "-") 
+        id_str = id_str.replace("'", "") 
+        id_str = id_str.replace(" ", "-") 
+        id_str = id_str.replace(" ", "") # remove all left spaces
+        id_str = id_str.strip("- ")
+        #TODO: remove multiple dashes
+        return id_str
+
+    @staticmethod
+    def color(ent_type):
+        if ent_type == 'PERSON': return 'red'
+        elif ent_type == 'ORG': return 'blue'
+        elif ent_type == 'GPE': return 'green'
+        elif ent_type == 'LOCATION': return 'green'
+        else: return 'gray'
+
+    def __eq__(self, other):
+        return self.id == other.id
+    
+    def __str__(self):
+        return str(self.__dict__)
+
+
+
+class Link:
+
+    def __init__(self, source, target, rel_type=None, label=None, props={}):
+        self.source = source
+        self.target = target
+        self.rel_type = rel_type
+        self.labels = []
+        if label is not None:
+            self.labels.add(label)
+        self.props = props
+
+
+    def add_label(self, label):
+        if label not in self.labels:
+            self.labels.append(label)
+
+    def __eq__(self, other):
+        return self.source == other.source and self.target == other.target
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+
+class NodeEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if issinstance(obj, Node):
+            return {
+                'ent_id': obj.ent1_id,
+                'ent_type': obj.ent_type,
+                'ent_label': obj.ent_label
+            }
+        return json.JSONEncoder(self, obj)
---
+++ kgbuild.py
@@ -0,0 +1,279 @@
+#!/usr/bin/python3
+
+import argparse
+import re
+import json
+import os
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
+from networkx.readwrite import json_graph
+#from gensim.models import KeyedVectors
+from sklearn.manifold import TSNE
+
+from entities import Node, Link
+
+
+class GraphBuilder:
+
+    def __init__(self, dataframe=None, entlinks=None, 
+            graph_path=None): 
+        """
+        :param dataframe: containing the dataset
+        :param entlinks: dictionnary representing the entities links
+        :graph_path: saved graph file path
+        """
+        if graph_path is not None:
+            self.load_graph(graph_path)
+        elif dataframe is not None:
+            self.G = nx.Graph() # should be multigraph
+            self.df = dataframe
+            self.entlinks = entlinks
+            self.nodes = []
+            self.links = []
+        else:
+            raise ValueError("Provide a dataframe or a path to load the graph")
+        
+
+
+    def build(self, self_links=True):
+        """
+        :param recursive_links: show self link or not
+        """
+        self.list_nodes_links()
+        G = nx.Graph()
+        for node in self.nodes:
+            G.add_node(node.id)
+            n = G.nodes[node.id]
+            n['type'] = node.ner_type
+            n['label'] = node.label
+            n['aliases'] = node.aliases
+        for link in self.links:
+            if not self_links and link.source.id == link.target.id: continue
+            G.add_edge(link.source.id, link.target.id)
+            e = G.edges[link.source.id, link.target.id]
+            e['labels'] = link.labels
+            e['type'] = link.rel_type
+        self.G = G
+        return self.G
+
+
+    def list_nodes_links(self):
+        nodes = []
+        links = []
+        for idx, row in self.df.iterrows():
+            subj_ent = row['SUBJ_ENT']
+            # TODO uncomment
+            #label = self.entlookup[subj_ent] if subj_ent in self.entlookup else subj_ent 
+            label = subj_ent
+            ner_type = row['SUBJ_ENT_TYPE']
+            source = Node(label, ner_type)
+            if source not in nodes:
+                nodes.append(source)
+            obj_ent = row['OBJ_ENT']
+            # TODO uncomment
+            #label = self.entlookup[obj_ent] if obj_ent in self.entlookup else obj_ent 
+            label = obj_ent
+            ner_type = row['OBJ_ENT_TYPE']
+            target = Node(label, ner_type)
+            if target not in nodes:
+                nodes.append(target)
+            link = Link(source, target, rel_type=row['RELATION_TYPE']) 
+            #link = Link(source, target) 
+            label = row['RELATION']
+            try:
+                idx = links.index(link)
+                links[idx].labels.append(label)
+            except ValueError:
+                link.labels.append(label)
+                links.append(link)
+        self.nodes = nodes
+        self.links = links
+        return self.nodes, self.links
+
+
+    def save_graph(self, filename, graph=None):
+        if graph is None:
+            data = json_graph.node_link_data(self.G)
+        else:
+            data = json_graph.node_link_data(graph)
+        file_content = json.dumps(data)
+        with open(filename, 'w') as outfile: 
+            outfile.write(file_content)
+    
+
+    def load_graph(self, filename):
+        file_content = ""
+        with open(filename, 'r') as infile:
+            file_content = infile.read()
+        data = json.loads(file_content)
+        self.G = json_graph.node_link_graph(data)
+
+
+    def embbedings_layout(self, embeddings_path, nodes=None):
+        # TODO 2D/3D and tsne/pca
+        if nodes is None:
+            nodes = G.nodes()
+        model = KeyedVectors.load(embeddings_path)
+        #vectors = [model.wv[node] for node in nodes]
+        tsne = TSNE(perplexity=40, n_components=2, init='pca', 
+                n_iter=2500, random_state=23)
+        zero = np.zeros_like(model.wv[0])
+        vectors = [model.wv[k] if k in model.wv else zero 
+                for k in model.wv.key_to_index]
+        lowd_vecs = tsne.fit_transform(vectors) 
+        nodes_pos = [lowd_vecs[model.wv.key_to_index[node]] 
+                if node in model.wv else [0,0]
+                for node in nodes]
+        pos = dict(zip(nodes, nodes_pos))
+        return pos
+
+
+    @staticmethod
+    def nodecolors(nodes):
+        # TODO improve like link
+        colormap = {"ORGANIZATION": "r", 
+            "PERSON":"g",
+            "LOCATION":"b",
+            "COMMODITY":"y",
+        }
+        colors = [colormap[nodes[key]['type']] 
+            if 'type' in nodes[key] and nodes[key]['type'] in colormap 
+            else "w" for key in nodes]
+        return colors
+
+
+    @staticmethod
+    def linkcolors(edges):
+        #print(edges[0])
+        reltypes = [d['type'] for _, d in edges.items()]
+        coldic = {rel:col for col, rel in enumerate(set(reltypes)) }
+        colors = [coldic[reltype] for reltype in reltypes]
+        return colors
+
+
+def kgbuild(df_path=None, graph_path=None, component=None, 
+        outpath=None, figure=None, wv_path=None, verbose=False):
+    builder = None
+    if df_path is None and graph_path is None:
+        raise ValueError("Provide a csv file path or a graph to load")
+    if graph_path is not None:
+        if verbose : print("Loading graph from file: {}".format(graph_path))
+        builder = GraphBuilder(graph_path=graph_path)
+    elif df_path is not None:
+        if verbose : print("Building graph from dataframe")
+        df = pd.read_csv(df_path)
+        builder = GraphBuilder(df)
+        builder.build(self_links=False)
+    G = builder.G
+    if component is not None:
+        if component < 0: 
+            raise ValueError("Component argumnent should be >= 0")
+        if verbose : print("Processing subgraph (from component {})".format(component))
+        compset = sorted(nx.connected_components(G), key=len, reverse=True)
+        G = G.subgraph(compset[component])
+    if outpath is not None:
+        if verbose : print("Saving to {}".format(outpath))
+        builder.save_graph(outpath, graph=G)
+    if figure is not None:
+        if verbose : print("Drawing figure to {}".format(figure))
+        if verbose: print("....Creating layout")
+        if wv_path is not None:
+            layout = builder.embbedings_layout(wv_path, G.nodes())
+        else:
+            layout = nx.spring_layout(G)
+        if verbose: print("....Drawing nodes")
+        plt.figure(figsize=(10,8))
+        #node_colors = GraphBuilder.nodecolors(G.nodes())
+        nodetypes = ['ORGANIZATION', 'PERSON', 'LOCATION', 'COMMODITY']
+        for nodetype in nodetypes:
+            if verbose: print(f"...Drawing {nodetype}")
+            allnodes = G.nodes()
+            nodes = [node for node in allnodes 
+                        if 'type' in allnodes[node] and allnodes[node]['type']== nodetype]
+            nodecolors = GraphBuilder.nodecolors(G.subgraph(nodes).nodes())
+            nx.draw_networkx_nodes(G, pos=layout, nodelist=nodes, 
+                    node_color=nodecolors, label=nodetype,
+                    node_size=25, linewidths=0.5)
+        #nx.draw_networkx_nodes(G, layout, node_size=25, linewidths=0.5,
+        #        node_color=node_colors)
+        if verbose: print("....Drawing edges")
+        link_colors = GraphBuilder.linkcolors(G.edges)
+        nx.draw_networkx_edges(G, layout, width=0.5, arrows=True, 
+                edge_color=link_colors, edge_cmap=plt.cm.tab20)
+        plt.legend(scatterpoints=1)
+        plt.savefig(figure, dpi=100)
+
+
+    
+
+def getargs():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-i', '--input', required=False, 
+            help="triples csv filepath")
+    group.add_argument('-l', '--load', required=False, 
+            help="input file of the network to load. Do not build from csv.")
+    parser.add_argument('-V', '--vectors', required=False, 
+            help="vectors file model.")
+    parser.add_argument('-o', '--output', required=False, 
+            help="Output file of the network")
+    parser.add_argument('-f', '--figure', required=False, 
+            help="Output file of the network figure")
+    parser.add_argument('-c', '--component', required=False, type=int,
+            help="use indexes to isolate a component of the graph 0 being the biggest")
+    parser.add_argument('-v', '--verbose', required=False, action="store_true",
+            help="verbose mode. showing operation being done")
+    args = parser.parse_args()
+    args_dict = dict()
+    args_dict['input'] = args.input
+    args_dict['output'] = args.output
+    args_dict['figure'] = args.figure
+    args_dict['component'] = args.component
+    args_dict['load'] = args.load
+    args_dict['vectors'] = args.vectors
+    args_dict['verbose'] = args.verbose
+    return args_dict
+
+# TODO add legend
+# TODO possibility to show labels
+    
+if __name__ == "__main__":
+    args = getargs()
+    kgbuild(df_path = args['input'], 
+            graph_path = args['load'], 
+            component = args['component'], 
+            outpath = args['output'], 
+            figure = args['figure'], 
+            wv_path = args['vectors'], 
+            verbose = args['verbose'])
+
+    """
+    triples_fpath = args['input']#"../data/triples/triples-elinked-rand-lbl.csv"
+    wv_fpath = args['vectors'] #"../data/gensim.55K.100.bin"
+    fig_fpath = args['figure']#"../data/figs/network.pdf"
+    df = pd.read_csv(triples_fpath)
+    builder = GraphBuilder(df)
+    builder.list_nodes_links()
+    G = builder.build(self_links=False)
+    compset = sorted(nx.connected_components(G), key=len, reverse=True)
+    giant = G.subgraph(compset[0])
+    giant_pos = builder.embbedings_layout( wv_fpath, giant.nodes())
+    node_colors = GraphBuilder.nodecolormap(giant.nodes())
+    plt.figure(figsize=(10,8))
+    nx.draw(G)
+    plt.savefig('../data/figs/net.pdf', dpi=100)
+    plt.clf()
+    nx.draw_networkx_nodes(giant, giant_pos, 
+        node_size=15, linewidths=0.3, 
+        node_color=node_colors, arrows=True)
+    nx.draw_networkx_edges(giant, giant_pos)
+    plt.savefig('../data/figs/net-giant.pdf', dpi=100)
+    plt.clf()
+    nx.draw(G.subgraph(compset[1]), arrows=True)
+    plt.savefig('../data/figs/comp1.pdf', dpi=100)
+    plt.clf()
+    plt.close()
+    #"""
+   
