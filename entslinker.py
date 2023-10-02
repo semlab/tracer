5d9c65724a62bcf0da521abbed4b88ea0f03f18d
godidier <godidier@gmail.com>
2023-10-03 01:20:50 +0900

    entity link reimplement"

--- entslinker.py
+++ entslinker.py
@@ -12,10 +12,43 @@ class EntityLinker:
 
     def __init__(self, dataframe):
         """ dataframe from triplex"""
+        self.pronouns = ["he", "she", "it", "they"]
         self.dataframe = dataframe
+        self.links_df = None
+        self.cleaned_df = None
         self.links = set() 
         self.lookup = dict()
         pass
+
+
+    def filter_out_pronouns(self, row):
+        subj_not_pronouns = row['SUBJ_ENT'] not in self.pronouns
+        obj_not_pronouns = row['OBJ_ENT'] not in self.pronouns
+        return subj_not_pronouns and obj_not_pronouns
+
+
+    def is_link(self, row):
+        subj_ent = row['SUBJ_ENT']
+        obj_ent = row['OBJ_ENT']
+        not_pronouns = subj_ent not in self.pronouns and obj_ent not in self.pronouns
+        same_entity_type = (row['SUBJ_ENT_TYPE'] == row['OBJ_ENT_TYPE']) 
+        be_relation = row['RELATION'] == 'be'
+        has_acronym = subj_ent.isupper() or obj_ent.isupper()
+        return not_pronouns and same_entity_type and be_relation and has_acronym # TODO make has_acronym optional
+
+
+    def is_not_link(self, row):
+        return not self.is_link(row)
+
+
+    def filter_links(self):
+        df = self.dataframe[self.dataframe.apply(self.filter_out_pronouns, axis=1)]
+        self.links_df = df[df.apply(self.is_link, axis=1)]
+        self.cleaned_df = df[df.apply(self.is_not_link, axis=1)]
+
+
+    def run(self):
+        self.filter_links()
 
 
     def find_links(self, keep_line=True):
@@ -23,12 +56,11 @@ class EntityLinker:
            keep_line: whether to remove the line representing a link
         """
         links = set()
-        pronouns = ["he", "she", "it", "they"]
         for idx, row in df.iterrows():
             subj = row['SUBJECT']
             obj = row['OBJECT']
             rel = row['RELATION']
-            not_pronouns = subj not in pronouns and obj not in pronouns
+            not_pronouns = subj not in self.pronouns and obj not in self.pronouns
             same_entity_type = (row['SUBJ_ENT_TYPE'] == row['OBJ_ENT_TYPE']) 
             entities_only = (row['SUBJECT'] == row['SUBJ_ENT'] and row['OBJECT'] == row['OBJ_ENT'])
             if (rel == 'be' and same_entity_type and 
@@ -39,6 +71,7 @@ class EntityLinker:
                     self.dataframe.drop(idx, inplace=True) # removing the line
         self.links = links
         return self.links
+
 
 
     def build_lookup(self):
@@ -85,14 +118,17 @@ def getargs():
     parser.add_argument('-i', '--input', required=True, 
             help="input triples csv file")
     parser.add_argument('-o', '--output', required=True, 
-            help="output with entity links filtered csv")
-    parser.add_argument('-l', '--save-links', required=False,
+            help="output with entity links filtered out csv")
+    parser.add_argument('--save-links', required=False,
             help="save the identified links")
+    parser.add_argument('--save-lookup', required=False,
+            help="save the lookup table for links")
     args = parser.parse_args()
     args_dict = dict()
     args_dict['input'] = args.input
     args_dict['output'] = args.output
     args_dict['save-links'] = args.save_links
+    args_dict['save-lookup'] = args.save_links
     return args_dict
 
 
@@ -102,16 +138,19 @@ if __name__ == "__main__":
     #triple_csvfile = "../data/triples/triples.csv"
     #triple_outfilepath = "../data/triples/triples-elinked.csv"
     #linkents_filepath="../data/linked-ents.json"
+    """
+    entslinkd, df = linkents(df)
+    with open(linkents_filepath, 'w') as f:
+        json.dump(entslinkd, f)
+    df.to_csv(triple_outfilepath, index=None)
+    """
+    """
     args = getargs()
     triple_csvfile = args['input']
     triple_outfilepath = args['output']
     links_outfilepath = args['save-links']
     df = pd.read_csv(triple_csvfile)
     """
-    entslinkd, df = linkents(df)
-    with open(linkents_filepath, 'w') as f:
-        json.dump(entslinkd, f)
-    df.to_csv(triple_outfilepath, index=None)
     """
     linker = EntityLinker(df)
     links = linker.find_links(keep_line=False)
@@ -124,6 +163,27 @@ if __name__ == "__main__":
             f.write(json.dumps(links_json))
     #print(links)
     print("Number of links {}".format(len(links)))
+    """
+
+    args = getargs()
+    inputpath = args['input']
+    outputpath = args['output']
+    linkspath = args['save-links']
+    lookuppath = args['save-lookup']
+    df = pd.read_csv(inputpath)
+
+    linker = EntityLinker(df)
+    linker.run()
+    linker.cleaned_df.to_csv(outputpath)
+    if linkspath is not None: # Save links df
+        linker.links_df.to_csv(linkspath)
+    if lookuppath:
+        pass # create lookup and save
+    
+    print(len(df))
+    print(len(linker.links_df))
+    print(len(linker.cleaned_df))
+
     
     
     
